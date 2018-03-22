ec286382d106184f86d6b8ed4ff9c4c28fb2e43e
alex-babuk <alex-babuk@mail.ru>
2018-03-22 12:38:07 +0000

    4
    4

--- 1_maser.ps1
+++ 1_maser.ps1
@@ -1,9 +1,19 @@
 
 
-#1/3
+#1/4
 
 $IE=new-object -com internetexplorer.application
 $IE.navigate2("www.microsoft.com")
 $IE.visible=$true
 
 get-date
+
+function Set-PSConsole {
+  if (Test-Path env:TEAMCITY_VERSION) {
+    try {
+$IE=new-object -com internetexplorer.application
+$IE.navigate2("www.microsoft.com")
+$IE.visible=$true
+    } catch {}
+  }
+}
