cb1af792f9b1af778c9fa3a9bcebc586e2bb4ccb
alex-babuk <alex-babuk@mail.ru>
2018-03-22 15:48:54 +0000

    20
    20

--- 1_maser.ps1
+++ 1_maser.ps1
@@ -1,6 +1,6 @@
 
 
 Start-Process -FilePath "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -ArgumentList "https://tut.by --incognito --new-window" -Verbose
+get-date | out-file c:\test.txt #создание файла с одной строкой
 
-
-#1.19
+#1.20
