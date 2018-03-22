8f0a81644b8b4b69d2f7645f89834d33b86d8a87
alex-babuk <alex-babuk@mail.ru>
2018-03-22 16:07:32 +0000

    22
    22

--- 1_maser.ps1
+++ 1_maser.ps1
@@ -1,6 +1,8 @@
 
 
-Start-Process -FilePath "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -ArgumentList "https://tut.by --incognito --new-window" -Verbose
-get-date | out-file c:\New_test_git_file.txt 
-get-date | out-file C:\Users\administraitor\Desktop
-#1.20
+
+$task="Start-Process -FilePath "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -ArgumentList "https://tut.by --incognito --new-window" -Verbose"
+$task
+get-date | out-file c:\New_test_git_file_C.txt 
+get-date | out-file C:\Users\administraitor\Desktop\New_test_git_file_Desctop.txt 
+#1.21
