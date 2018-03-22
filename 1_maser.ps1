47a6919a7abe33abdd2565e6de2c83688c762622
alex-babuk <alex-babuk@mail.ru>
2018-03-22 13:53:38 +0000

    12
    12

--- 1_maser.ps1
+++ 1_maser.ps1
@@ -1,7 +1,6 @@
 
 
-#1.11
-#new-
 Start-Process -FilePath "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -ArgumentList "https://tut.by --incognito --new-window"
 get-date
-
+get-chrome
+#1.12
