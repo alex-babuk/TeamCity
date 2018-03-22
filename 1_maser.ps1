f59760bdbec91cc6f95f0050978c7294471cf777
alex-babuk <alex-babuk@mail.ru>
2018-03-22 13:14:46 +0000

    1/8
    8

--- 1_maser.ps1
+++ 1_maser.ps1
@@ -1,8 +1,7 @@
 
 
-#1.7
-#new-object
-
-chrome.exe "http://nagios.com/"
+#1.8
+#new-
+Start-Process -FilePath "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -ArgumentList "https://tut.by --incognito --new-window"
 get-date
 
